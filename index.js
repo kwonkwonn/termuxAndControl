25f2806ca1981dd36ac24cabdba0295d118cd409
Kwon <ggiicchh48@gmail.com>
2024-10-04 18:36:21 +0900

    basic axios

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-
-
-function main(){
-const axios = require('axios');
-
-
-try{
-	const response= await axios.get("http://google.com");
-	console.log(response.data);
-}catch(error){
-	console.error(error);
-}
+function main() {
+  const axios = require("axios");
+  getData(axios);
 }
 
+main();
+async function getData(axios) {
+  try {
+    const response = await axios.get("http://google.com");
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
 }
